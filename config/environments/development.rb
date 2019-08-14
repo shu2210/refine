266a07763419ea6c9f387f1154e55d596b7cf1df
shuto.yasunaga <shuto.yasunaga@lt-s.jp>
2019-08-14 12:11:50 +0900

    install better errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # enable docker to run byebug
   config.web_console.whitelisted_ips = '0.0.0.0/0'
+  # better errors config
+  BetterErrors::Middleware.allow_ip! '0.0.0.0/0'
 end
