292d228941f08cb0a8522c10ae985cd8ed70e302
shuto.yasunaga <shuto.yasunaga@lt-s.jp>
2019-08-15 08:55:59 +0900

    create home screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#index'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/controllers/homes_controller_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HomesController, type: :controller do
+  describe 'GET #index' do
+    subject { get :index }
+
+    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to render_template(:index) }
+  end
+end
