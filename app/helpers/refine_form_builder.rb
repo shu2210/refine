6ed85d0aa95a1666a9c9ce9df4b28cad2db0c4be
shuto.yasunaga <shuto.yasunaga@lt-s.jp>
2019-08-23 08:32:07 +0900

    refactoring refine_form_builder

--- app/helpers/refine_form_builder.rb
+++ app/helpers/refine_form_builder.rb
@@ -1,37 +1,37 @@
 # frozen_string_literal: true
 
 class RefineFormBuilder < ActionView::Helpers::FormBuilder
-  delegate :content_tag, :concat, to: :@template
+  delegate :concat, to: :@template
 
   def icon_field(icon)
-    @template.content_tag :div, class: 'uk-inline uk-width-1-1' do
-      concat @template.content_tag(:span, '', class: 'uk-form-icon', 'uk-icon': "icon: #{icon}")
+    @template.tag.div class: 'uk-inline uk-width-1-1' do
+      concat @template.tag.span '', class: 'uk-form-icon', 'uk-icon': "icon: #{icon}"
       concat yield
     end
   end
 
   def email_field_with_icon(attribute, options = {})
-    @template.content_tag :div, class: 'uk-margin' do
+    @template.tag.div class: 'uk-margin' do
       concat icon_field(:mail) { email_field(attribute, options) }
       concat error_message(attribute)
     end
   end
 
   def password_field_with_icon(attribute, options = {})
-    @template.content_tag :div, class: 'uk-margin' do
+    @template.tag.div class: 'uk-margin' do
       concat icon_field(:lock) { password_field(attribute, options) }
       concat error_message(attribute)
     end
   end
 
   def submit(label, options = {})
-    @template.content_tag(:div, class: 'actions uk-margin') { super(label, options) }
+    @template.tag.div(class: 'actions uk-margin') { super(label, options) }
   end
 
   private
 
   def error_message(attribute)
-    @template.content_tag :span, class: 'uk-text-small uk-text-danger' do
+    @template.tag.span class: 'uk-text-small uk-text-danger' do
       @object.errors.full_messages_for(attribute).first
     end
   end
