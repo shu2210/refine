86051a5437546e377275cef29d9a75f22d6f4778
yasunaga shuto <yasunaga.shuto@gmail.com>
2019-09-05 20:53:36 +0900

    mod social login button to vue component

---
+++ app/javascript/packs/components/sns/SocialLoginButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="uk-inline uk-width-1-1 uk-margin-bottom">
+    <span class="uk-form-icon" :uk-icon="provider"></span>
+    <a :href="url" class="uk-button uk-button-default uk-margin-small-right uk-width-1-1">
+      {{ provider }}でログイン
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    provider: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
