0c7d9af18259b4aeb4bb0f980e60744a9679c374
yasunaga shuto <yasunaga.shuto@gmail.com>
2019-09-21 14:14:48 +0900

    create code detail

--- app/javascript/packs/code.js
+++ app/javascript/packs/code.js
@@ -1,9 +1,11 @@
 import Vue from 'vue/dist/vue.esm.js'
 import GoodBadButton from './components/code/GoodBadButton.vue'
+import CodeArea from './components/code/CodeArea.vue'
 
 new Vue({
   el: '#code',
   components: {
-    GoodBadButton
+    GoodBadButton,
+    CodeArea
   }
 })
