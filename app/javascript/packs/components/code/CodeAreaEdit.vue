30d8d2bb6398d977f70e35c29e7b405c32c465d1
yasunaga shuto <yasunaga.shuto@gmail.com>
2019-09-29 15:19:07 +0900

    dynamically change mode

--- app/javascript/packs/components/code/CodeAreaEdit.vue
+++ app/javascript/packs/components/code/CodeAreaEdit.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="code">
     <div class="language-select uk-width-1-1 uk-text-right uk-margin">
-      <select name="lang" class="uk-select uk-width-1-4">
+      <select name="lang" class="uk-select uk-width-1-4" @change="changeMode">
         <option v-for="mode, name in languages" :value="mode">{{ name }}</option>
       </select>
     </div>
@@ -17,26 +17,56 @@ export default {
       languages: {
         'Ruby': 'ruby',
         'Javascript': 'javascript',
+        'TypeScript': 'typescript',
         'Python': 'python',
         'PHP': 'php',
-        'Java': 'java'
-      }
+        'Perl': 'perl',
+        'HTML': 'html',
+        'XML': 'xml',
+        'erb': 'html_ruby',
+        'slim': 'slim',
+        'CSS': 'css',
+        'SCSS': 'scss',
+        'SASS': 'sass',
+        'stylus': 'stylus',
+        'Java': 'java',
+        'C': 'c',
+        'C++': 'c_pp',
+        'Go': 'golang',
+        'Kotlin': 'kotlin',
+        'Swift': 'swift',
+        'Lisp': 'lisp',
+        'Scala': 'scala',
+        'COBOL': 'cobol',
+        'SQL': 'sql',
+        'MySQL': 'mysql',
+        'PostgreSQL': 'pgsql',
+        'SQLServer': 'sqlserver',
+        'JSON': 'json',
+        'Shell': 'sh',
+      },
+      editor: null
     }
   },
   mounted: function () {
-    var editor = ace.edit('editor', {
+    this.editor = ace.edit('editor', {
       theme: 'ace/theme/textmate',
       mode: 'ace/mode/ruby',
       autoScrollEditorIntoView: true,
-      minLines: 10,
+      maxLines: 30,
+      minLines: 25,
     });
+  },
+  methods: {
+    changeMode: function (event) {
+      this.editor.session.setMode('ace/mode/' + event.target.value);
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #editor {
-  min-height: 400px;
-  height: 100%;
+  height: 400px;
 }
 </style>
