24e1bda481e73dcf66496e9910e085da3a1ffb14
yasunaga shuto <yasunaga.shuto@gmail.com>
2019-10-05 16:36:38 +0900

    add modal component

---
+++ app/javascript/packs/components/common/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :id="id" class="uk-flex-top" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      <h2 class="uk-modal-title">{{ title }}</h2>
+      <button class="uk-modal-close-default" type="button" uk-close></button>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    id: String
+  }
+}
+</script>
