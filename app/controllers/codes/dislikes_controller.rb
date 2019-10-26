1b1d57f691b83b654538a62e76580437c2b1ad17
yasunaga shuto <yasunaga.shuto@gmail.com>
2019-10-26 16:59:13 +0900

    add code dislikes create

--- app/controllers/codes/dislikes_controller.rb
+++ app/controllers/codes/dislikes_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Codes::DislikesController < ApplicationController
+  protect_from_forgery except: :create
+  before_action :authenticate_user!
+
+  def create
+    code = Code.find(params[:code_id])
+    like = CodeDislike.new(user: current_user, code: code)
+    like.save!
+    render json: { status: :success }
+  rescue StandardError => e
+    logger.error e
+    render json: { status: :error }
+  end
 end
