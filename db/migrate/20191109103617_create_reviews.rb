812271b0b1617e4803fa8c4de4d2cc465da3e624
yasunaga shuto <yasunaga.shuto@gmail.com>
2019-11-09 19:43:37 +0900

    create review model

---
+++ db/migrate/20191109103617_create_reviews.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :code_id
+      t.integer :line
+      t.text :review
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_043106) do
+ActiveRecord::Schema.define(version: 2019_11_09_103617) do
 
   create_table "code_dislikes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 2019_10_26_043106) do
   create_table "languages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "mode", comment: "モード"
     t.string "name", comment: "言語名"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "code_id"
+    t.integer "line"
+    t.text "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
