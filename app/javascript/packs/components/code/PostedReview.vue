f569cae60a5714a437512b65fe8822e81e1291df
shu2210 <yasunaga.shuto@gmail.com>
2020-02-26 22:26:03 +0900

    add animation to delete review

--- app/javascript/packs/components/code/PostedReview.vue
+++ app/javascript/packs/components/code/PostedReview.vue
@@ -1,35 +1,37 @@
 <template>
-  <tr>
-    <td colspan="2">
-      <div class="posted-review uk-flex">
-        <div class="uk-flex-first uk-width-1-6 uk-text-center">
-          <img :src="icon" class="uk-border-circle uk-width-1-3" />
-        </div>
-        <div class="review-info uk-width-3-6">
-          <div class="user-name uk-margin">
-            {{ userName }}
+  <transition name="fade">
+    <tr v-if="show">
+      <td colspan="2">
+        <div class="posted-review uk-flex">
+          <div class="uk-flex-first uk-width-1-6 uk-text-center">
+            <img :src="icon" class="uk-border-circle uk-width-1-3" />
           </div>
-          <div class="description">
-            <p>
-              {{ review }}
-            </p>
+          <div class="review-info uk-width-3-6">
+            <div class="user-name uk-margin">
+              {{ userName }}
+            </div>
+            <div class="description">
+              <p>
+                {{ review }}
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="control-area uk-width-1-6 uk-text-center" v-if="canEdit">
-          <a class="uk-margin-small-right" uk-icon="pencil"></a>
-          <a :href="'#confirm' + id" class="uk-margin-small-right" uk-icon="trash" uk-toggle></a>
-          <a class="uk-margin-small-right" uk-icon="menu"></a>
-        </div>
-        <modal title="確認" :id="'confirm' + id">
-          <p class="uk-margin">削除すると元に戻せません。よろしいですか？</p>
-          <div class="uk-text-right">
-            <button class="uk-button uk-button-default uk-modal-close" type="button">キャンセル</button>
-            <button class="uk-button uk-button-primary" type="button" @click="deleteReview">削除</button>
+          <div class="control-area uk-width-1-6 uk-text-center" v-if="canEdit">
+            <a class="uk-margin-small-right" uk-icon="pencil"></a>
+            <a :href="'#confirm' + id" class="uk-margin-small-right" uk-icon="trash" uk-toggle></a>
+            <a class="uk-margin-small-right" uk-icon="menu"></a>
           </div>
-        </modal>
-      </div>
-    </td>
-  </tr>
+          <modal title="確認" :id="'confirm' + id">
+            <p class="uk-margin">削除すると元に戻せません。よろしいですか？</p>
+            <div class="uk-text-right">
+              <button class="uk-button uk-button-default uk-modal-close" type="button">キャンセル</button>
+              <button class="uk-button uk-button-primary" type="button" @click="deleteReview">削除</button>
+            </div>
+          </modal>
+        </div>
+      </td>
+    </tr>
+  </transition>
 </template>
 
 <script>
@@ -57,12 +59,19 @@ export default {
       default: false
     }
   },
+  data: function () {
+    return {
+      show: true
+    }
+  },
   methods: {
     deleteReview: function () {
       axios.delete('/reviews/' + this.id).then((response) => {
         if(response.data['status'] == 'success') {
-          this.$destroy();
-          this.$el.parentNode.removeChild(this.$el);
+          this.show = false;
+          // TODO: fade後、削除
+          // this.$destroy();
+          // this.$el.parentNode.removeChild(this.$el);
         }
         UIkit.modal('#confirm' + this.id).hide();
         console.log(response.status);
@@ -76,3 +85,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
