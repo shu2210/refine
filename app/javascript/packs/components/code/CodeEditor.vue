2429559d1a7cac4b9da6b9962a23ee89577f1249
shu2210 <yasunaga.shuto@gmail.com>
2020-03-07 20:44:50 +0900

    mod code editor

--- app/javascript/packs/components/code/CodeEditor.vue
+++ app/javascript/packs/components/code/CodeEditor.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="editor">
+    <div class="language-select uk-width-1-1 uk-text-right uk-margin">
+      <select name="code[][language_id]" class="uk-select uk-width-1-4" @change="changeMode($event)">
+        <option v-for="lang in langs"
+                :value="lang['id']"
+                :key="lang['id']">
+          {{ lang['name'] }}
+        </option>
+      </select>
+    </div>
+
     <monaco-editor
       :language="language"
       height="400"
@@ -17,17 +27,28 @@ import MonacoEditor from 'monaco-editor-vue';
 
 export default {
   props: {
-    code: String,
-    language: String,
+    langs: Array,
     errors: Array
   },
   data() {
     return {
+      language: 'ruby',
       editCode: ''
     }
   },
   created() {
     this.editCode = this.code;
+  },
+  methods: {
+    changeMode(event) {
+      var mode = '';
+      var vm = this;
+      this.langs.forEach(function(lang){
+        if(lang['id'] == event.target.value) {
+          vm.language = lang['mode'];
+        }
+      });
+    }
   },
   components: {
     MonacoEditor
--- app/javascript/packs/components/code/CodeForm.vue
+++ app/javascript/packs/components/code/CodeForm.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="code">
-    <div class="language-select uk-width-1-1 uk-text-right uk-margin">
-      <select name="code[][language_id]" class="uk-select uk-width-1-4" @change="changeMode($event)">
-        <option v-for="lang in langs"
-                :value="lang['id']"
-                :key="lang['id']">
-          {{ lang['name'] }}
-        </option>
-      </select>
-    </div>
     <div class="uk-margin">
       <code-editor
-        :language="language"
+        :langs="langs"
         :errors="errors"
       />
     </div>
@@ -26,24 +17,9 @@ import CodeEditor from './CodeEditor.vue';
 
 export default {
   props: {
+    codes: String,
     langs: Array,
     errors: Array
-  },
-  data() {
-    return {
-      language: "ruby"
-    }
-  },
-  methods: {
-    changeMode(event) {
-      var mode = '';
-      var vm = this;
-      this.langs.forEach(function(lang){
-        if(lang['id'] == event.target.value) {
-          vm.language = lang['mode'];
-        }
-      });
-    }
   },
   components: {
     CodeEditor
