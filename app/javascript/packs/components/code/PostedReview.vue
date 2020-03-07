0cb3e0436f6460a183e4687502dc9cbbbdc3b000
shu2210 <yasunaga.shuto@gmail.com>
2020-03-07 17:28:51 +0900

    add update review feature

--- app/javascript/packs/components/code/PostedReview.vue
+++ app/javascript/packs/components/code/PostedReview.vue
@@ -2,7 +2,14 @@
   <transition name="fade">
     <tr v-if="show">
       <td colspan="2">
-        <div class="posted-review uk-flex">
+        <div class="edit-review" v-if="mode == 'edit'">
+          <vue-simplemde v-model="review" ref="markdownEditor" />
+          <div class="uk-text-right uk-margin">
+            <button class="uk-button uk-button-default" @click="cancelReview">キャンセル</button>
+            <button class="uk-button uk-button-primary" @click="updateReview">更新</button>
+          </div>
+        </div>
+        <div class="posted-review uk-flex" v-else>
           <div class="uk-flex-first uk-width-1-6 uk-text-center">
             <img :src="icon" class="uk-border-circle uk-width-1-3" />
           </div>
@@ -15,7 +22,7 @@
             </div>
           </div>
           <div class="control-area uk-width-1-6 uk-text-center" v-if="canEdit">
-            <a class="uk-margin-small-right" uk-icon="pencil"></a>
+            <a class="uk-margin-small-right" uk-icon="pencil" @click="switchEditMode"></a>
             <a :href="'#confirm' + id" class="uk-margin-small-right" uk-icon="trash" uk-toggle></a>
             <a class="uk-margin-small-right" uk-icon="menu"></a>
           </div>
@@ -38,6 +45,7 @@ import Modal from '../common/Modal.vue';
 import marked from 'marked/marked.min.js';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github-gist.css';
+import VueSimplemde from 'vue-simplemde';
 
 export default {
   props: {
@@ -62,7 +70,8 @@ export default {
   },
   data() {
     return {
-      show: true
+      show: true,
+      mode: 'view'
     }
   },
   created() {
@@ -79,6 +88,23 @@ export default {
     }
   },
   methods: {
+    switchEditMode() {
+      this.mode = 'edit';
+    },
+    switchViewMode() {
+      this.mode = 'view';
+    },
+    cancelReview() {
+      this.switchViewMode();
+    },
+    updateReview() {
+      axios.put('/reviews/' + this.id, { review: this.review }).then((response) => {
+        this.switchViewMode();
+        console.log(response.status);
+      }, (error) => {
+        console.log(error);
+      });
+    },
     deleteReview() {
       axios.delete('/reviews/' + this.id).then((response) => {
         if(response.data['status'] == 'success') {
@@ -95,7 +121,8 @@ export default {
     }
   },
   components: {
-    Modal
+    Modal,
+    VueSimplemde
   }
 }
 </script>
