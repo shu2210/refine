9b1ac9640d24585bcc89de90bc7b7cca26ea5787
shu2210 <yasunaga.shuto@gmail.com>
2020-03-14 21:30:44 +0900

    add comment spec

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,19 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
-  def create; end
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: { status: :success, comment: comment }
+    else
+      render json: { status: :error, message: comment.errors.full_messages }
+    end
+  end
+
+  private
+
+  def comment_params
+    params[:comments][:user_id] = current_user.id
+    params.require(:comments).permit(:review_id, :user_id, :comment)
+  end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -12,12 +12,37 @@ RSpec.describe CommentsController, type: :controller do
     end
 
     context 'ログインしている場合' do
+      let!(:user) { create(:user) }
+      let!(:review) { create(:review) }
+
+      before { sign_in user }
+
       context 'エラーがある場合' do
-        it 'エラーになる'
+        let!(:params) { { comments: { comment: '', review_id: review.id } } }
+
+        it 'status = errorが返る' do
+          post :create, params: params
+          status = JSON.parse(response.body)['status']
+          expect(status).to eq('error')
+        end
+
+        it 'commentsのレコードは作成されない' do
+          expect { post :create, params: params }.to change(Comment, :count).by(0)
+        end
       end
 
       context 'エラーがない場合' do
-        it 'commentsのレコードを作成する'
+        let!(:params) { { comments: { comment: 'test', review_id: review.id } } }
+
+        it 'status = successが返る' do
+          post :create, params: params
+          status = JSON.parse(response.body)['status']
+          expect(status).to eq('success')
+        end
+
+        it 'commentsのレコードを作成する' do
+          expect { post :create, params: params }.to change(Comment, :count).by(1)
+        end
       end
     end
   end
