5c8fa7775f4983fd840564482a0cc30a3cf672c8
shu2210 <yasunaga.shuto@gmail.com>
2020-03-14 21:36:00 +0900

    add comment spec

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,5 +2,8 @@
 
 FactoryBot.define do
   factory :comment do
+    user { build(:user) }
+    review { build(:review) }
+    comment { Faker::Lorem.characters(number: 10) }
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:comment) { build(:comment) }
+
+  it '正常にビルドできること' do
+    expect(comment).to be_valid
+  end
+
+  describe 'comment' do
+    context '空の場合' do
+      let(:comment) { build(:comment, comment: '') }
+
+      it { expect(comment).to be_invalid }
+    end
+  end
 end
