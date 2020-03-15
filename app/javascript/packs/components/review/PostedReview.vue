33048032e987d9a64794a8bac1a1cc5ef2eb61f5
shu2210 <yasunaga.shuto@gmail.com>
2020-03-15 19:23:06 +0900

    change layout posted review

--- app/javascript/packs/components/review/PostedReview.vue
+++ app/javascript/packs/components/review/PostedReview.vue
@@ -14,19 +14,20 @@
         <div class="posted-review uk-flex" v-else>
           <div class="uk-flex-first uk-width-1-6 uk-text-center">
             <img :src="icon" class="uk-border-circle uk-width-1-3" />
-          </div>
-          <div class="review-info uk-width-3-6">
             <div class="user-name uk-margin">
               {{ userName }}
             </div>
-            <div class="description">
-              <p v-html="markedReview"></p>
-            </div>
           </div>
-          <div class="control-area uk-width-1-6 uk-text-center" v-if="canEdit">
-            <a class="uk-margin-small-right" uk-icon="pencil" @click="switchEditMode"></a>
-            <a :href="'#confirm' + id" class="uk-margin-small-right" uk-icon="trash" uk-toggle></a>
-            <a class="uk-margin-small-right" uk-icon="menu"></a>
+          <div class="uk-width-expand">
+            <div class="control-area uk-width-1-1 uk-text-right uk-margin" v-if="canEdit">
+              <a class="uk-margin-small-right" uk-icon="pencil" @click="switchEditMode"></a>
+              <a :href="'#confirm' + id" class="uk-margin-small-right" uk-icon="trash" uk-toggle></a>
+            </div>
+            <div class="review-info uk-width-1-1">
+              <div class="description">
+                <p v-html="markedReview"></p>
+              </div>
+            </div>
           </div>
           <modal title="確認" :id="'confirm' + id">
             <p class="uk-margin">削除すると元に戻せません。よろしいですか？</p>
