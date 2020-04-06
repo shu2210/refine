b6ecc7e7a30e977055fe9480eaf8ab7af907356e
shu2210 <yasunaga.shuto@gmail.com>
2020-04-06 20:00:37 +0900

    add history column to user_codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_125019) do
+ActiveRecord::Schema.define(version: 2020_04_06_105506) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -99,6 +99,8 @@ ActiveRecord::Schema.define(version: 2020_03_28_125019) do
 
   create_table "user_codes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", comment: "ユーザーID"
+    t.integer "parent_id"
+    t.integer "version"
     t.string "status", comment: "ステータス"
     t.string "title", comment: "タイトル"
     t.string "description", comment: "説明"
