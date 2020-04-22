d37337157976bb47a208650779606566949415e8
shu2210 <yasunaga.shuto@gmail.com>
2020-04-22 20:03:28 +0900

    add code_spec

--- spec/models/code_spec.rb
+++ spec/models/code_spec.rb
@@ -5,12 +5,25 @@ require 'rails_helper'
 RSpec.describe UserCode, type: :model do
   describe 'code' do
     let(:code) { build(:code, code: code_value) }
-    before { code.valid? }
 
-    context '空白の場合' do
-      let(:code_value) { '' }
+    context 'postの場合' do
+      before { code.valid? }
 
-      it { expect(code.errors[:code]).not_to be_empty }
+      context '空白の場合' do
+        let(:code_value) { '' }
+
+        it { expect(code.errors[:code]).not_to be_empty }
+      end
+    end
+
+    context 'draftの場合' do
+      before { code.valid?(:draft) }
+
+      context '空白の場合' do
+        let(:code_value) { '' }
+
+        it { expect(code.errors[:code]).to be_empty }
+      end
     end
   end
 end
