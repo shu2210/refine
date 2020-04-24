a28f5cc14e179e2e927ac5fd3d2753772adcc4d2
shu2210 <yasunaga.shuto@gmail.com>
2020-04-24 20:28:56 +0900

    add association to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
+  has_many :user_code_tags
   has_many :user_codes, through: :user_code_tags
 
   validates :name, presence: true, length: { maximum: 100 }
