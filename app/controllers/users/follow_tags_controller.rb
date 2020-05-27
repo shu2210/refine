409799261b278c37b8d90f0a09550095cdd2d7b3
shu2210 <yasunaga.shuto@gmail.com>
2020-05-27 20:55:29 +0900

    create new action

--- app/controllers/users/follow_tags_controller.rb
+++ app/controllers/users/follow_tags_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Users::FollowTagsController < ApplicationController
+  def new; end
 end
--- spec/controllers/users/follow_tags_controller_spec.rb
+++ spec/controllers/users/follow_tags_controller_spec.rb
@@ -3,4 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Users::FollowTagsController, type: :controller do
+  describe 'GET #new' do
+    subject { get :new }
+
+    it { is_expected.to have_http_status(:ok) }
+    it { is_expected.to render_template(:new) }
+  end
 end
