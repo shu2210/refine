ff32f05beb6868b61db9545af3f05955d978b0df
shu2210 <yasunaga.shuto@gmail.com>
2020-05-28 21:15:33 +0900

    add sign_in context

--- app/controllers/users/follow_tags_controller.rb
+++ app/controllers/users/follow_tags_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Users::FollowTagsController < ApplicationController
+  before_action :authenticate_user!
+
   def new; end
 end
--- spec/controllers/users/follow_tags_controller_spec.rb
+++ spec/controllers/users/follow_tags_controller_spec.rb
@@ -4,9 +4,21 @@ require 'rails_helper'
 
 RSpec.describe Users::FollowTagsController, type: :controller do
   describe 'GET #new' do
-    subject { get :new }
+    context 'ログインしていない場合' do
+      subject { get :new }
 
-    it { is_expected.to have_http_status(:ok) }
-    it { is_expected.to render_template(:new) }
+      it { is_expected.to have_http_status(:redirect) }
+      it { is_expected.to redirect_to('/users/sign_in') }
+    end
+
+    context 'ログインしている場合' do
+      let(:user) { create(:user) }
+      before { sign_in user }
+
+      subject { get :new }
+
+      it { is_expected.to have_http_status(:ok) }
+      it { is_expected.to render_template(:new) }
+    end
   end
 end
